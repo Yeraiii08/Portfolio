4adc70ac08ad9189f35ea3280600c357de08b21d
Yeraiii08 <yeraiigarcia3@gmail.com>
2026-04-13 00:04:15 +0200

    Primer componente añadido

---
+++ src/componentes/Inicio.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Inicio = () => {
+  return (
+    <section id="hero" className="hero-section">
+      <img src="#"/>
+      <h1>Hola, soy Yerai</h1>
+      <p>Desarrollador de Software</p>
+    </section>
+  );
+};
+
+export default Inicio;
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Sobre mí</a></li>
+        <li><a href="#">Proyectos</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
